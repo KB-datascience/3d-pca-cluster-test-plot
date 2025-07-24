3f787a1f5e25d79c83a01c4e9cd26a57a997dd63
KB <119862839+KB-datascience@users.noreply.github.com>
2025-07-24 14:02:05 +0100

    app.py

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ import pandas as pd
 # Load the PCA-clustered dataset
 df = pd.read_csv("df_pca_4_test_scaled_4.csv")
 
-# Define a manual color mapping for clusters
-cluster_color_map = {
-    '0': '#1f77b4',  # Blue
-    '1': '#ff7f0e',  # Orange
-    '2': '#2ca02c',  # Green
-    '3': '#d62728'   # Red
-}
-
-# Ensure 'Cluster' is string
-df_pca_4_test_scaled_4['Cluster'] = df_pca_4_test_scaled_4['Cluster'].astype(str)
-
 fig = px.scatter_3d(
     df_pca_4_test_scaled_4,
     x='PC1', y='PC2', z='PC3',
